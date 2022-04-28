5cd6e7f0b30a13221786e9b667a7c8c40f208550
diegodze <72428573+diegodze@users.noreply.github.com>
2022-04-28 18:57:32 -0500

    github pages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.diego.app',
   appName: 'chat',
   webDir: 'www',
   bundledWebRuntime: false
